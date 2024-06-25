1c40c00745855a1dcacb82375ee07aa7e9fb38d3
SubhamayDebnath <subhamaydebnath6@gmail.com>
2024-06-25 22:10:29 +0530

    Write a function that removes the third element from an array and returns a new array with the removed element. Do not modify the original array

--- js/assignment-2/question_9.js
+++ js/assignment-2/question_9.js
@@ -4,15 +4,19 @@ const arr=[10,20,30,40,50,60,70]
 
 const newArr=[];
 
-const removedArr=(arr)=>{
-    arr.forEach((element,index) => {
-        if (index != 2){
-            newArr.push(element)
-        }
-    });
-    return newArr
-}
+// const removedArr=(arr)=>{
+//     arr.forEach((element,index) => {
+//         if (index != 2){
+//             newArr.push(element)
+//         }
+//     });
+//     return newArr
+// }
+const removedArr = arr
+                        .map((element,index)=> index !== 2 ? element:null)
+                        .filter(element=>element!==null)
+
 console.log("New Array after remove the third element");
-console.log(removedArr(arr))
+console.log(removedArr)
 console.log("\nOriginal array");
 console.log(arr)
